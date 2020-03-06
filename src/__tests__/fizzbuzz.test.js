42361cb19b2dbca19c4054a08508ede88aef96c2
Aurélien Bouteiller <aurelien.bouteiller@ynov.com>
2020-03-06 12:12:47 +0100

    RED: Test number divisible by 3 return Fizz

--- src/__tests__/fizzbuzz.test.js
+++ src/__tests__/fizzbuzz.test.js
@@ -3,3 +3,7 @@ const resolve = require("../main/fizzbuzz");
 test("should return empty string if number not divisible by 3 and not divisible by 5", () => {
   expect(resolve(4)).toBe("");
 });
+
+test("should return Fizz if number divisible by 3", () => {
+  expect(resolve(3)).toBe("Fizz");
+});
