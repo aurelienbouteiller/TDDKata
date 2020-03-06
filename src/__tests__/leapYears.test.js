6fca8616144292a61e4829e83423bf930c72e6e8
Aurélien Bouteiller <aurelien.bouteiller@ynov.com>
2020-03-06 14:21:57 +0100

    RED: Test year divisible by 4 and not by 100 return true

--- src/__tests__/leapYears.test.js
+++ src/__tests__/leapYears.test.js
@@ -7,3 +7,7 @@ test("should return true if year is divisible by 400", () => {
 test("should return false if year is divisible by 100 and not by 400", () => {
   expect(isLeapYear(500)).toBe(false);
 });
+
+test("should return true if year is divisible by 4 and not by 100", () => {
+  expect(isLeapYear(2008)).toBe(true);
+});
