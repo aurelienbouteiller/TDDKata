16ec3c024bfc26a2b16489e51a512853ccaab65d
Aurélien Bouteiller <aurelien.bouteiller@ynov.com>
2020-03-06 14:24:42 +0100

    REFACTOR: Test year divisible by 4 and not by 100 return true

--- src/main/leapYears.js
+++ src/main/leapYears.js
@@ -1,11 +1,7 @@
 function isLeapYear(year) {
   if (year % 4 === 0 && year % 100 !== 0) {
     return true;
-  } else if (year % 400 === 0) {
-    return true;
-  } else {
-    return false;
-  }
+  } else return year % 400 === 0;
 }
 
 module.exports = isLeapYear;
