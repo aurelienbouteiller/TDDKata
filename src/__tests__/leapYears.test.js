ae45dc71a913ac4c3130916e13ef6ffb294ba5cf
Aurélien Bouteiller <aurelien.bouteiller@ynov.com>
2020-03-06 14:19:11 +0100

    GREEN: Test year divisible by 100 and not by 400 return false

--- src/__tests__/leapYears.test.js
+++ src/__tests__/leapYears.test.js
@@ -3,3 +3,7 @@ const isLeapYear = require("../main/leapYears");
 test("should return true if year is divisible by 400", () => {
   expect(isLeapYear(2000)).toBe(true);
 });
+
+test("should return false if year is divisible by 100 and not by 400", () => {
+  expect(isLeapYear(500)).toBe(false);
+});
