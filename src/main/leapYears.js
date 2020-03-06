5b6be313341b3168736d6e82f560f2db6a666f96
Aurélien Bouteiller <aurelien.bouteiller@ynov.com>
2020-03-06 14:24:11 +0100

    GREEN: Test year divisible by 4 and not by 100 return true

--- src/main/leapYears.js
+++ src/main/leapYears.js
@@ -1,5 +1,11 @@
 function isLeapYear(year) {
-  return year % 400 === 0;
+  if (year % 4 === 0 && year % 100 !== 0) {
+    return true;
+  } else if (year % 400 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = isLeapYear;
